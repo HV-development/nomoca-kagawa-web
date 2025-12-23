021461e5ea24646dfe2f991d794128548a4ffb61
YutaWatanabe-hv <yuta.watanabe@hornet-v.com>
2025-12-24 06:07:40 +0900

    fix: FVのスマホ画像を下にずらして背景の緑をはみ出す位置に調整

--- frontend/src/app/lp/page.tsx
+++ frontend/src/app/lp/page.tsx
@@ -344,7 +344,7 @@ export default function LPPage() {
               height={578}
               className="w-64 md:w-[400px] lg:w-[500px] h-auto"
             />
-            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-[10%]">
               <Image
                 src="/lp/images/fv-phone.png"
                 alt="スマホ画面"
