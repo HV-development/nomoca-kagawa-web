d2f7d63496167221b535a3a724d2f2f300642be0
Rina-hornet-v <rina.nishigaki@hornet-v.com>
2026-05-28 18:33:00 +0900

    frontend/src/schemas/auth.ts戻し

---
+++ frontend/src/schemas/auth.ts
@@ -0,0 +1,13 @@
+export {
+  // スキーマのエクスポート
+  UserRegistrationRequestSchema,
+  UseRregistrationCompleteSchema,
+  otpRequestSchema,
+  otpVerifySchema,
+
+  // 型のエクスポート
+  type UserRegistrationRequest,
+  type UserRegistrationComplete,
+  type OtpRequest,
+  type OtpVerify,
+} from '@hv-development/schemas';
